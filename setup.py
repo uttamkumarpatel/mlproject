75c715a1a3b75037a0c99704bd55ede0fc73f712
Uttam Kumar <uk2242004@gmail.com>
2025-05-02 18:31:47 +0530

    Your meaningful commit message here

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Krish',
+    author='Uttam',
     author_email='uk2242004@gmail.com',
     packages=find_packages(),
     install_requires = get_requirements('requirements.txt')
